2b5338511e670c4dcf246b0514298fb93d44a812
Crew Spence <crewspence@gCrewSpence.local>
2021-05-23 05:20:40 -0400

    Wrote working solution.

--- codewars_average_scores.js
+++ codewars_average_scores.js
@@ -2,6 +2,10 @@
 Create a function that returns the average of an array of numbers ("scores"), rounded to the nearest whole number. You are not allowed to use any loops (including for, for/in, while, and do/while loops).
 */
 
-function average(scores) {
-    // code to calculate the average
+const average = (scores) => {
+    let total = scores.reduce(
+        ( accumulator, currentValue ) => accumulator + currentValue,
+        0
+      );
+    return Math.round(total / scores.length);
 }
